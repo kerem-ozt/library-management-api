3138da80b72113bf8d56c7fa8d9b9f1e0d9abd11
Kerem Öztürk <ichbinichbin285@gmail.com>
2024-11-30 15:10:31 +0300

    updated getUser service for requested return data format

--- Services/Users.js
+++ Services/Users.js
@@ -25,13 +25,54 @@ class UserService {
 
     static async getUser(id, language) {
         try {
-            const user = await db.Users.findByPk(id);
-
-            if (!user) return { type: false, data: [], message: LanguageHelper(language, 'get_user_not_found') };
-            return { type: true, data: user, message: LanguageHelper(language, 'get_user_success')};
-        } 
-        catch (error) {
-            return { type: false, message: error.message };
+          const user = await db.Users.findByPk(id, {
+            attributes: ['id', 'name'],
+            include: [
+              {
+                model: db.Borrows,
+                include: [
+                  {
+                    model: db.Books,
+                    attributes: ['name'],
+                  },
+                ],
+              },
+            ],
+          });
+    
+          if (!user) {
+            return { type: false, data: [], message: LanguageHelper(language, 'get_user_not_found') };
+          }
+    
+          const books = {
+            past: [],
+            present: [],
+          };
+    
+          user.Borrows.forEach((borrow) => {
+            if (borrow.return_date) {
+              books.past.push({
+                name: borrow.Book.name,
+                userScore: borrow.rating,
+              });
+            } else {
+              books.present.push({
+                name: borrow.Book.name,
+              });
+            }
+          });
+    
+          return {
+            type: true,
+            data: {
+              id: user.id,
+              name: user.name,
+              books,
+            },
+            message: LanguageHelper(language, 'get_user_success'),
+          };
+        } catch (error) {
+          return { type: false, message: error.message };
         }
     }
     
